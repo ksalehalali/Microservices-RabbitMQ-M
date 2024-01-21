e8abc640e953e267dac9c68feae3948202e1871b
Mohamed Ebrahim Atia <eng.mohamed@hotmail.ca>
2024-01-21 09:13:07 +0200

    remove un wanted line of code

--- Core/RabbitMq/BusConfiguration/RabbitMqBus.cs
+++ Core/RabbitMq/BusConfiguration/RabbitMqBus.cs
@@ -20,7 +20,6 @@ namespace Core.RabbitMq.BusConfiguration
             return Bus.Factory.CreateUsingRabbitMq(cfg =>
             {
                 
-                cfg.UseMessageRetry(retry => retry.Interval(5, 1000));
                 cfg.Host(new Uri(@"rabbitmq://" + RabbitMqSetting.HostName + @"/"), hst =>
                 {
                     hst.Username(RabbitMqSetting.UserName);
